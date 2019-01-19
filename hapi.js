4b83898d22c346debb6fbfaac6a5e23d87648885
svngoku <32180057+svngoku@users.noreply.github.com>
2019-01-19 16:36:52 +0100

    Update hapi.js

--- hapi.js
+++ hapi.js
@@ -1,14 +1,14 @@
-const api = require('hapi')
+const api = require('hapi');
 const Server = new Hapi.Server();
 
 Server.connection({ 
   "host" : "localhost", 
   "port" : 3000 , 
-  "routes" : { "cors" : true }
-  Server.start( error => {
+  "routes" : { "cors" : true });
+Server.start(error => {
     if(error) {
       throw error;
     }
     console.log('listen at ${Server.info.uri}');    
-  })
+  });
 
