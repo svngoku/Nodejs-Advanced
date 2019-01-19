05ea14346e55a9125985fa3366ddd849a0e43573
svngoku <32180057+svngoku@users.noreply.github.com>
2019-01-19 16:27:48 +0100

    Create hapi.js

---
+++ hapi.js
@@ -0,0 +1,14 @@
+const api = require('hapi')
+const Server = new Hapi.Server();
+
+Server.connection({ 
+  "host" : "localhost", 
+  "port" : 3000 , 
+  "routes" : { "cors" : true }
+  Server.start( error => {
+    if(error) {
+      throw error;
+    }
+    console.log('listen at ${Server.info.uri}');    
+  })
+
