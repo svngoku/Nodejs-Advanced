5f88b716e5b12d18af629ae937c41ece9d31c720
Svngoku <marymarty104@gmail.com>
2019-07-12 01:06:10 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const { createServer } = require('http');
 const Router = require('./url/router');
-// const ecstatic = require('ecstatic');
-
 const router = new Router();
-const defaultHeaders = { "content-type": "Text/plain" };
+const {port} = {
+    hostname: 'localhost',
+    port: 8080
+};
+const message = { 
+    message : 'Hello World' 
+};
 
-router.add('GET', '/home', (req,res) => {
-    res.json('hello world');
+const server = createServer((req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Content-Type", "application/json");
+    res.status = 200;
+    res.end(JSON.stringify(message));
 });
+
+
+
+server.listen(port);
 
 
